4fd1bd841f3df89f43f797b9778f9f47159af8b1
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-09 03:06:01 +0300

    Fix error 500

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 export async function getServerSideProps({ res }) {
   res.writeHead(307, { Location: "/public" });
   res.end();
+  return { props: {} };
 }
 
 function RedirectPage() {
